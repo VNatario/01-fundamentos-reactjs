49b64a567fc62f486400366b323a1dc0b6882456
Vitor Natario <natariovitor@gmail.com>
2023-06-07 14:31:47 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Vitor Natario"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dolor enim quia eligendi id accusantium possimus nesciunt consectetur deserunt vel. At totam et aperiam iste rerum, inventore excepturi facere obcaecati."
+          />
+          <Post
+            author="Vitor Natario"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dolor enim quia eligendi id accusantium possimus nesciunt consectetur deserunt vel. At totam et aperiam iste rerum, inventore excepturi facere obcaecati."
+          />
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { PencilSimpleLine } from "@phosphor-icons/react";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img src="https://github.com/VNatario.png" className={styles.avatar} />
+        <strong>Vitor Natario</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilSimpleLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
